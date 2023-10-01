cc89b2241e2c5e591b95bac1bfa6d133dd6d1f56
Jason Kuo <jasonkuo147@gmail.com>
2023-09-30 20:57:15 -0500

    Some sort of colliders

--- PlayerController.gd
+++ PlayerController.gd
@@ -1,10 +1,15 @@
-extends Node2D
+class_name PlayerController
+extends CharacterBody2D
 
 @export var speed = 100
+var direction: Vector2
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var x_direction = Input.get_axis("ui_left", "ui_right") * Vector2.RIGHT
 	var y_direction = Input.get_axis("ui_down", "ui_up") * Vector2.UP
-	var direction = (x_direction + y_direction).normalized()
-	position += delta * direction * speed
+	direction = (x_direction + y_direction).normalized()
+
+func _physics_process(delta):
+	velocity = direction * speed
+	move_and_slide()
